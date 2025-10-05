49b0283d0368c5d2e813e8343696f61328d856eb
dvkahh <devikajithimanoj@gmail.com>
2025-10-05 21:08:41 +0530

    Added code for directory.py

--- src/common/directory.py
+++ src/common/directory.py
@@ -1,1 +1,18 @@
+class Directory:
+    def __init__(self):
+        self.entries = {}
+
+    def add_entry(self, filename, inode_number):
+        if filename in self.entries:
+            raise Exception("File already exists!")
+        self.entries[filename] = inode_number
+
+    def remove_entry(self, filename):
+        if filename in self.entries:
+            del self.entries[filename]
+        else:
+            raise Exception("File not found!")
+
+    def list_entries(self):
+        return list(self.entries.items())
 
