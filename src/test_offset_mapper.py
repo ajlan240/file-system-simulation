3053977966e009d44685844592616f610e3171b7
Vaishnav Ceh <65798585+vaishnavceh@users.noreply.github.com>
2025-10-06 23:02:42 +0530

    Delete src/test_offset_mapper.py

--- src/test_offset_mapper.py
+++
@@ -1,6 +0,0 @@
-import unittest
-from src.fileio.offset_mapper import logical_to_block_offset
-
-class TestOffsetMapper(unittest.TestCase):
-    def test_offset(self):
-        self.assertEqual(logical_to_block_offset(1024, 512), 2)
