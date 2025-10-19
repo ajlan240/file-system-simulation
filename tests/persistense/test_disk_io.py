53488a1b6a7eccd8d6b9681b3db556cfbf21a6a6
athilazubair <148233468+athilazubair@users.noreply.github.com>
2025-10-19 12:19:10 +0530

    Delete tests/persistense directory

--- tests/persistense/test_disk_io.py
+++
@@ -1,9 +0,0 @@
-# test_disk_io.py
-from persistence import disk_io
-
-def test_disk_io():
-    data = b"HelloPersistence"
-    disk_io.write_block(2, data)
-    read_data = disk_io.read_block(2)
-    assert data in read_data
-    print("[TEST] Disk I/O test passed.")
