7b773c3283deb7f0f99172057f4d5429cac5e3e8
athilazubair <148233468+athilazubair@users.noreply.github.com>
2025-10-19 12:19:34 +0530

    Delete tests/test_unmount.py

--- tests/test_unmount.py
+++
@@ -1,7 +0,0 @@
-# test_unmount.py
-from persistence import unmount
-
-def test_unmount():
-    metadata = {"mounted": True, "info": "Unmount test run"}
-    unmount.unmount(metadata)
-    print("[TEST] Unmount test passed.")
